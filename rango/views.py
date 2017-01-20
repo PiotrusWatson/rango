b49174b70895f32342436b287bb256756bbc3cf5
Piotrus Watson <piotruswatson@gmail.com>
2017-01-20 15:11:07 +0000

    almost did 4 aside from media shenanigans

--- rango/views.py
+++ rango/views.py
@@ -7,5 +7,4 @@ def index(request):
 
     return render(request, 'rango/index.html', context=context_dict)
 def about(request):
-    return HttpResponse("WELCOME TO THE ABOUT PAGE :) TODAY I WOULD LIKE TO DISCUSS SPROTS. WHAT IS YOUR FAVOURITE SPROT?"
-                        "\n MINES FENCE. I AM BIG FAN OF FENCE. IT IS IN THE STABBING WHERE THE ENJOYMENT IS.")
+    return render(request, 'rango/about.html')
