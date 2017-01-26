c124e74bc32d71a1ed3f3921d2d0373532ca47bf
Piotrus Watson <piotruswatson@gmail.com>
2017-01-26 16:25:16 +0000

    HI SO I'M AT 6 NOW

--- populate_rango.py
+++ populate_rango.py
@@ -1,5 +1,5 @@
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tango_with_django_project.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", 'tango.settings')
 
 import django
 django.setup()
@@ -31,34 +31,38 @@ def populate():
          "url": "http://flask.pocoo.org"}
     ]
 
-    cats = {"Python": {"pages": python_pages},
-            "Django": {"pages": django_pages},
-            "Other Frameworks": {"pages": other_pages}}
+    cats = {"Python": {"pages": python_pages, "views": 128, "likes": 64},
+            "Django": {"pages": django_pages, "views": 64, "likes": 32},
+            "Other Frameworks": {"pages": other_pages, "views": 32, "likes": 16}}
 
     """OK SO now we go through the cats dictionary, and add literally everything"""
 
     for cat, cat_data in cats.iteritems():
-        c = add_cat(cat)
-        for p in cat_data["pages"]:
+        c = add_cat(cat, cat_data["views"], cat_data["likes"]) #adds a cat to a database
+        for p in cat_data["pages"]: #then loops through and adds a page under each category
             add_page(c, p["title"], p["url"])
     #PRINTING
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
-            print("- {0} - {1}".format(str(c). str(p)))
+            print("- {0} - {1}".format(str(c), str(p)))
 
 def add_page(cat, title, url, views=0):
+    """This adds a page to the database, using the page model, sets its attributes and then saves it"""
     p = Page.objects.get_or_create(category=cat, title=title)[0]
     p.url=url
     p.views=views
     p.save()
     return p
 
-def add_cat(name):
+def add_cat(name, views, likes):
+    """see: add_page but with categories :)"""
     c = Category.objects.get_or_create(name=name)[0]
+    c.views = views
+    c.likes = likes
     c.save()
     return c
 
 #IT BEGINS
-if __name__=="__main__":
+if __name__=="__main__": #If run as usual
     print("STARTING RANGO POPULATION SCRIPT...")
     populate()
